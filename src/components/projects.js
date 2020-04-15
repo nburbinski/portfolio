592d1a7dcc1cc4b96373c6675d69cf1fbc11942f
nburbinski <nburbinski@gmail.com>
2020-04-15 15:36:37 -0400

    edit project list

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,94 +3,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import projectStyles from "./projects.module.scss"
+import { projects } from "../projects"
 
 const Projects = () => {
-  const projects = [
-    {
-      Title: "Animal Shelter App",
-      Description:
-        "A mobile app that provides the user with pet information from their local animal shelter. Users can create an account for their local shelter and add animals to a public list. ",
-      Tech: ["React Native", "Redux", "ReactJS", "Firebase"],
-      Demo: "https://expo.io/@nburbinski/animal_shelter",
-      Repo: "https://github.com/nburbinski/animal_shelter_rn",
-      imgs: [],
-    },
-    {
-      Title: "Plant Trackr",
-      Description:
-        "A solution to the impossible and relatable task of managing dozens of household plants, Plant Trackr allows you to keep track of all of your plants needs to stay happy and healthy. Login: Username: Demo User PW: Demo",
-      Tech: [
-        "MongoDB",
-        "Mongoose",
-        "Express",
-        "ReactJS",
-        "NodeJS",
-        "jwt",
-        "REST API",
-      ],
-      Demo: "http://plantcity.herokuapp.com/",
-      Repo: "https://github.com/nburbinski/plant_city",
-      imgs: "react-icon.png",
-    },
-    {
-      Title: "UCF Movie Night",
-      Description:
-        "As a resolution to the daunting task of picking a movie for movie night, I created a web app that allows you to search and store movies. You can then select a movie at random to watch",
-      Tech: [
-        "MongoDB",
-        "Mongoose",
-        "Express",
-        "ReactJS",
-        "NodeJS",
-        "Bootstrap",
-        "GraphQL",
-      ],
-      Demo: "https://ucfmovietime.herokuapp.com/",
-      Repo: "https://github.com/nburbinski/ucf_movie_time",
-      imgs: [],
-    },
-    // {
-    //   Title: "Grocery Time*",
-    //   Description:
-    //     "Picking groceries can be difficult, especially when trying to meal prep. Grocery Time is a web app compiles your groceries whether individually or by recipe and gives you nutritional data through the Spoonacular API",
-    //   Tech: ["PostgreSQL", "Express", "ReactJS", "NodeJS", "SASS", "Redux"],
-    //   Demo: "",
-    //   Repo: "https://github.com/nburbinski/grocerytime",
-    //   imgs: [],
-    // },
-    {
-      Title: "Portfolio",
-      Description:
-        "A portfolio site to demonstrate my latest projects, as well as my resume, and a contact form.",
-      Tech: ["ReactJS", "GatsbyJS", "SASS"],
-      Demo: "",
-      Repo: "https://github.com/nburbinski/portfolio",
-      imgs: [],
-    },
-  ]
-
   return (
     <div className={projectStyles.projectsContainer}>
       <div className={projectStyles.title}>
         <h2> Projects </h2>
         <ol>
-          <li>
-            <Link to="/#Animal Shelter App">Animal Shelter App</Link>
-          </li>
-
-          <li>
-            <Link to="/#Plant Trackr">Plant Trackr</Link>
-          </li>
-          <li>
-            <Link to="/#UCF Movie Night">UCF Movie Night</Link>
-          </li>
-
-          {/* <li>
-            <Link to="/#Grocery Time">Grocery Time</Link>
-          </li> */}
-          <li>
-            <Link to="/#Portfolio">Portfolio</Link>
-          </li>
+          {projects.map(project => (
+            <li key={project.Title}>
+              <Link to={`/#${project.Title}`}>{project.Title}</Link>
+            </li>
+          ))}
         </ol>
       </div>
       <div className={projectStyles.projects}>
---
+++ src/projects.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    Title: "Animal Shelter App",
+    Description:
+      "A mobile app that provides the user with pet information from their local animal shelter. Users can create an account for their local shelter and add animals to a public list. ",
+    Tech: ["React Native", "Redux", "ReactJS", "Firebase"],
+    Demo: "https://expo.io/@nburbinski/animal_shelter",
+    Repo: "https://github.com/nburbinski/animal_shelter_rn",
+    imgs: [],
+  },
+  {
+    Title: "Plant Trackr",
+    Description:
+      "A solution to the impossible and relatable task of managing dozens of household plants, Plant Trackr allows you to keep track of all of your plants needs to stay happy and healthy. Login: Username: Demo User PW: Demo",
+    Tech: [
+      "MongoDB",
+      "Mongoose",
+      "Express",
+      "ReactJS",
+      "NodeJS",
+      "jwt",
+      "REST API",
+    ],
+    Demo: "http://plantcity.herokuapp.com/",
+    Repo: "https://github.com/nburbinski/plant_city",
+    imgs: "react-icon.png",
+  },
+  {
+    Title: "UCF Movie Night",
+    Description:
+      "As a resolution to the daunting task of picking a movie for movie night, I created a web app that allows you to search and store movies. You can then select a movie at random to watch",
+    Tech: [
+      "MongoDB",
+      "Mongoose",
+      "Express",
+      "ReactJS",
+      "NodeJS",
+      "Bootstrap",
+      "GraphQL",
+    ],
+    Demo: "https://ucfmovietime.herokuapp.com/",
+    Repo: "https://github.com/nburbinski/ucf_movie_time",
+    imgs: [],
+  },
+  {
+    Title: "Artsy Fartsy",
+    Description: "A site that provides random art located at NYC's MET",
+    Tech: ["ReactJS", "CSS"],
+    Demo: "https://laughing-meninsky-294e01.netlify.app/",
+    Repo: "https://github.com/nburbinski/art-project",
+    imgs: [],
+  },
+  {
+    Title: "Portfolio",
+    Description:
+      "A portfolio site to demonstrate my latest projects, as well as my resume, and a contact form.",
+    Tech: ["ReactJS", "GatsbyJS", "SASS"],
+    Demo: "",
+    Repo: "https://github.com/nburbinski/portfolio",
+    imgs: [],
+  },
+]
